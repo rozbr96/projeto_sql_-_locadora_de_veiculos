8c5b6ebc8f323bc19bc5adbb93de6924dfb22fc8
Rosemilson <rozbr96@gmail.com>
2023-10-13 15:52:19 -0300

    Add task 11 solution file

---
+++ tasks/11.sql
@@ -0,0 +1,6 @@
+-- Construa uma query SQL para editar o ano do automóvel com nome Fiat Cronos de “2022” para “2019” da tabela de CARS
+
+UPDATE cars
+SET year = 2019
+WHERE name = 'Fiat Cronos'
+  AND year = 2022;
