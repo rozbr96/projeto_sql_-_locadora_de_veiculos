c26b8d3a987d69088e97293afb1c5c346d36e1a5
Rosemilson <rozbr96@gmail.com>
2023-10-13 15:46:06 -0300

    Add task 09 file

---
+++ tasks/09.sql
@@ -0,0 +1,5 @@
+-- Construa uma query SQL para editar o campo e-mail do cliente com nome Carolina, onde devemos trocar de “carol@ig.com.br” para “carolina@campuscode.com.br”
+
+UPDATE customers
+SET email = 'carolina@campuscode.com.br'
+WHERE name = 'Carolina';
